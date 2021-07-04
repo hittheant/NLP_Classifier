cf63a72546b161eed264f490a6e226b89ea4c224
adyantbalaji <hittheant@gmail.com>
2021-07-05 00:04:57 +0530

    changed from cwt to dwt

--- mite/filters/wavelet/WaveletTransformFilter.py
+++ mite/filters/wavelet/WaveletTransformFilter.py
@@ -5,19 +5,20 @@ from mite.filters import AbstractBaseFilter
 
 class WaveletTransformFilter(AbstractBaseFilter):
     """ A Python implementation of a wavelet transform filter """
-    def __init__(self, scales=range(120, 128), wavelet_name='morl'):
-        self.scales = scales
+    def __init__(self, wavelet_name='db8'):
         self.wavelet_name = wavelet_name
 
     def filter(self, x):
         result = []
-        coeff, freq = pywt.cwt(x, self.scales, self.wavelet_name, 1)
-        result.extend(coeff.flatten())
+        x = np.transpose(x)
+        (cA, cD) = pywt.dwt(x, self.wavelet_name, 1)
+        result.extend(cA.flatten())
+        result.extend(cD.flatten())
         return result
 
 
 if __name__ == '__main__':
-    data = np.random.rand( 100, 6 )
+    data = np.random.rand(100, 6)
     cwt = WaveletTransformFilter()
-    features = cwt.filter( data )
-    print( np.array(features).shape )
+    features = cwt.filter(data)
+    print(np.array(features).shape)
